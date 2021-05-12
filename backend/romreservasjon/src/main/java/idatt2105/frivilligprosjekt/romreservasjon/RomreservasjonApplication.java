ec0531af9e6eab417a68bb77a2a93c75ec2ee6d2
magnbred <magnbred@stud.ntnu.no>
2021-05-12 10:08:27 +0200

    created initial folders

---
+++ backend/romreservasjon/src/main/java/idatt2105/frivilligprosjekt/romreservasjon/RomreservasjonApplication.java
@@ -0,0 +1,13 @@
+package idatt2105.frivilligprosjekt.romreservasjon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RomreservasjonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RomreservasjonApplication.class, args);
+	}
+
+}
---
+++ backend/romreservasjon/src/test/java/idatt2105/frivilligprosjekt/romreservasjon/RomreservasjonApplicationTests.java
@@ -0,0 +1,13 @@
+package idatt2105.frivilligprosjekt.romreservasjon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RomreservasjonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
