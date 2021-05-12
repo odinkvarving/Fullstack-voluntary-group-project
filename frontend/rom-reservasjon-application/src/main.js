c724f656de2ee35960de971ffa02b14f2ae8b659
Mattias Eggen <mattias.a.eggen@gmail.com>
2021-05-12 14:57:05 +0200

    Added Vuetify

--- frontend/rom-reservasjon-application/src/main.js
+++ frontend/rom-reservasjon-application/src/main.js
@@ -1,10 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
 import store from './store/index'
+import vuetify from './plugins/vuetify'
 
 Vue.config.productionTip = false
 
 new Vue({
   render: h => h(App),
-  store: store,
+  vuetify,
+  store: store
 }).$mount('#app')
