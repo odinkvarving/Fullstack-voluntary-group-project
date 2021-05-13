f1a631ba1e23487d634365a3c1bfbcdfd5b77580
Odin <odinkva@stud.ntnu.no>
2021-05-13 14:54:35 +0200

    Started making full-stack for AccountController

---
+++ backend/romreservasjon/src/test/java/idatt2105/frivilligprosjekt/romreservasjon/controllers/AccountControllerTest.java
@@ -0,0 +1,24 @@
+package idatt2105.frivilligprosjekt.romreservasjon.controllers;
+
+import org.junit.Before;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+public class AccountControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Before
+    void setup() {
+
+    }
+}
