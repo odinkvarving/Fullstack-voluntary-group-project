216ffe4d8681131e0e69997051d00906b0bdf6e7
Mattias Eggen <mattias.a.eggen@gmail.com>
2021-05-14 15:53:34 +0200

    Styling on LoginBox

--- frontend/rom-reservasjon-application/src/components/LoginComponents/LoginBox.vue
+++ frontend/rom-reservasjon-application/src/components/LoginComponents/LoginBox.vue
@@ -1,55 +1,61 @@
 <template>
-  <div id="login-page">
-    <v-container>
-      <SuccessPopUp class="popup" v-show="isPopUpVisible" :message="message" />
-      <ErrorPopUp
-        class="popup"
-        v-show="isErrorVisible"
-        v-on:close-popup="closePopUp"
-        :message="message"
-      />
-      <v-row>
-        <v-col cols="12">
-          <h1>Velkommen tilbake</h1>
-          <p class="white--text text--secondary">Logg på kontoen din</p>
-        </v-col>
-        <v-col cols="12">
-          <p class="text">Skriv inn e-post</p>
-          <v-text-field
-            label="E-post"
-            v-model="emailInput"
-            v-on:keyup.enter="loginAccount"
-            outlined
-            color="green"
-          ></v-text-field>
-        </v-col>
-        <v-col cols="12">
-          <p class="text">Skriv inn passord</p>
-          <v-text-field
-            type="password"
-            label="Passord"
-            v-model="passwordInput"
-            v-on:keyup.enter="loginAccount"
-            outlined
-            color="green"
-          ></v-text-field>
-        </v-col>
-        <v-col cols="6">
-          <p
-            @click="() => this.$router.push('/forgotpassword')"
-            class="green--text"
-            style="cursor: pointer"
-          >
-            Glemt passord?
-          </p>
-        </v-col>
-        <v-col cols="6" align="end">
-          <v-btn color="green" @click="loginAccount"
-            ><span>Logg inn</span></v-btn
-          >
-        </v-col>
-      </v-row>
-    </v-container>
+  <div>
+        <SuccessPopUp
+          v-show="isPopUpVisible"
+          :message="message"
+        />
+        <ErrorPopUp
+          v-show="isErrorVisible"
+          v-on:close-popup="closePopUp"
+          :message="message"
+        />
+    <div id="login-page">
+      <v-container>
+        <v-row>
+          <v-col cols="12">
+            <h1>Velkommen tilbake</h1>
+            <p class="white--text text--secondary">Logg på kontoen din</p>
+          </v-col>
+          <v-col cols="12">
+            <p class="text">Skriv inn e-post</p>
+            <v-text-field
+              label="E-post"
+              v-model="emailInput"
+              v-on:keyup.enter="loginAccount"
+              outlined
+              color="green"
+              :error-messages="eMailErrorMessage"
+            ></v-text-field>
+          </v-col>
+          <v-col cols="12">
+            <p class="text">Skriv inn passord</p>
+            <v-text-field
+              type="password"
+              label="Passord"
+              v-model="passwordInput"
+              v-on:keyup.enter="loginAccount"
+              outlined
+              color="green"
+              :error-messages="passwordErrorMessage"
+            ></v-text-field>
+          </v-col>
+          <v-col cols="6">
+            <p
+              @click="() => this.$router.push('/forgotpassword')"
+              class="green--text"
+              style="cursor: pointer"
+            >
+              Glemt passord?
+            </p>
+          </v-col>
+          <v-col cols="6" align="end">
+            <v-btn color="green" @click="loginAccount"
+              ><span>Logg inn</span></v-btn
+            >
+          </v-col>
+        </v-row>
+      </v-container>
+    </div>
   </div>
 </template>
 <script>
@@ -71,6 +77,8 @@ export default {
       isPopUpVisible: false,
       isErrorVisible: false,
       message: "",
+      eMailErrorMessage: "",
+      passwordErrorMessage: ""
     };
   },
 
@@ -79,11 +87,9 @@ export default {
       this.isEmailValid = true;
       this.isPopUpVisible = false;
       this.isErrorVisible = false;
-      if (this.emailInput === "" || this.passwordInput === "") {
-        document.getElementsById("login-page").style.filter = "blur(5px)";
-        this.message = "E-post- eller passordfeltet kan ikke være tomt";
-        this.isErrorVisible = true;
-        return;
+
+      if (!this.validateInput()) {
+          return;
       }
       if (!this.validateEmail()) {
         document.getElementById("login-page").style.filter = "blur(5px)";
@@ -94,6 +100,21 @@ export default {
       if (this.isEmailValid) {
         this.login();
       }
+    },
+
+    validateInput() {
+        var isValid = true;
+        if (this.emailInput === "") {
+            this.isEmailValid = false;
+            this.eMailErrorMessage = "Dette feltet kan ikke stå tomt";
+            isValid = false;
+        }
+        if (this.passwordInput === "") {
+            this.isPasswordValid = false;
+            this.passwordErrorMessage = "Dette feltet kan ikke stå tomt";
+            isValid = false;
+        }
+        return isValid;
     },
 
     validateEmail() {
