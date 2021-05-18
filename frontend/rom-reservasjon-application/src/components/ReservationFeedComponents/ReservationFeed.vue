7b331d395c10474ef5176ada1d9993867da072cb
scottrs <scottrs@ntnu.no>
2021-05-18 23:55:17 +0200

    Merged with reservationfeed and added folder

---
+++ frontend/rom-reservasjon-application/src/components/ReservationFeedComponents/ReservationFeed.vue
@@ -0,0 +1,76 @@
+<template>
+    <div class="reservation-feed">
+        <div class="reservation-feed-container">
+            <h1>Romreservasjoner</h1>
+            <div v-if="isDataReady && rooms">
+                <div class="feed" v-for="r in reservations" :key="r.id" @click="handleReservationClicked(r)">
+                    <ReservationBox :reservation="r" :rooms="rooms"/>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+<script>
+import ReservationBox from "./ReservationBox.vue";
+import { reservationService } from "../../services/ReservationService.js";
+
+/**
+ * ReservationFeed is a component which represents the reservation feed visible for admins only
+ * 
+ * @author Scott Rydberg Sonen
+ */
+
+export default {
+    name: "ReservationFeed",
+
+    components: {
+        ReservationBox
+    },
+
+    data() {
+        return {
+            reservations: [],
+            rooms: [],
+            isDataReady: false
+        }
+    },
+
+    /**
+     * mounted declares reservations and rooms, so the arrays can be passed as props to ReservationBox.
+     * isDataReady is a flag which turns true when both reservations and rooms arrays are fetched.
+     */
+    async mounted() {
+        this.reservations = await reservationService.getReservations();
+        this.rooms = this.$store.getters.getRooms;
+        while (!this.reservations) {
+            this.isDataReady = false;
+        }
+        while (!this.rooms) {
+            this.isDataReady = false;
+        }
+        this.isDataReady = true;
+    },
+
+    methods: {
+        /**
+         * handleReservationClicked is a function which redirects the admin to reservation page.
+         * The id is passed through the URL path.
+         */
+        handleReservationClicked(reservation) {
+            this.$router.push("/reservations", {id: reservation.id}); 
+        }
+    }
+}
+</script>
+<style scoped>
+    .reservation-feed {
+        background-color: #192138;
+        padding: 80px;
+    }
+    h1 {
+        color: white;
+    }
+    .feed {
+        cursor: pointer;
+    }
+</style>
