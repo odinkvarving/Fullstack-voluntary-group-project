8b154f032f5c9e23534284c948b1c3a4b40f06be
magnbred <magnbred@stud.ntnu.no>
2021-05-20 08:23:53 +0200

    styled reservation section

--- frontend/rom-reservasjon-application/src/components/Reservations/ReservationsOverview.vue
+++ frontend/rom-reservasjon-application/src/components/Reservations/ReservationsOverview.vue
@@ -1,11 +1,12 @@
 <template>
   <div class="reservation-overview">
       <h2>{{ computedDateFormattedDatefns }}</h2>
+      <div class="line"></div>
       <div class="time-list">
           <div class="timestamp" v-for="(timestamp, index) in timeList" :key="index">
               <p> {{timestamp}}</p>
-              <div class="time-box" v-if="!freeReservations[index].disabled" style="background-color: green"></div>
-              <div class="time-box" v-else style="background-color: red"></div>
+              <div class="time-box" v-if="!freeReservations[index].disabled" style="background-color: #192138;"></div>
+              <div class="time-box" v-else style="background-color: #C33838"></div>
           </div>
       </div>
   </div>
@@ -35,20 +36,45 @@ export default {
     .reservation-overview{
         border: 2px solid #01AB55;
         border-radius: 4px;
-        width: 400px;
+        width: 100%;
+        height: 800px;
+        background-color: #222B45;
+    }
 
+    .reservation-overview h2{
+        margin: 15px 0 15px 4%;
+    }
+
+    .time-list{
+        margin-top: 20px;
     }
 
     .timestamp{
         display: flex;
+        align-items: center;
+        margin-left: 5%;
+    }
+
+    .timestamp p {
+        margin: 0;
     }
 
     .time-box{
         width: 80%;
         border: 1px solid #01AB55;
+        opacity: 70%;
+        height: 40px;
+        margin-left: 5%;
     }
 
     .date-picker{
         width: 80%;
+    }
+
+    .line{
+        width: 90%;
+        margin: auto;
+        height: 1px;
+        background-color: #01AB55;
     }
 </style>
